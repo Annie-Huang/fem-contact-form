8b3183d4b5979497840173dba55867074f889cfd
Annie Huang <annie.hn@gmail.com>
2025-09-13 13:55:47 +1000

    Add boilerplate for form.js

---
+++ form.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+const firstNameField = document.getElementById('fname');
+
+function validateField(field) {}
+
+form.addEventListener('submit', (e) => {});
